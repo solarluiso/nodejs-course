b89dc44ba451b34b7fdc209b38c1991205c39475
Luis <solarluiso@gmail.com>
2024-11-04 10:33:34 -0500

    http module

---
+++ 1. node-tutorial/6. http-module/routes.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url == "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Homepage");
+  } else if (url === "/projects") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Projects");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("This page can not be found!");
+  }
+});
+
+port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
