1f28bf71fa7962dcbbce4ee250b2faf38d86cb30
Luis <solarluiso@gmail.com>
2024-11-10 21:17:39 -0500

    registration, login with jwt & role based authentication with middleware.

--- 7. nodejs-auth/controllers/auth-controller.js
+++ 7. nodejs-auth/controllers/auth-controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // register controller
 const registerUser = async (req, res) => {
@@ -53,4 +54,55 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+// login controller
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // find if the current user exists in database or not
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: `User doesn't exists`,
+      });
+    }
+    // if the password is correct or not
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials!",
+      });
+    }
+
+    // create user token
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+        username: user.username,
+        role: user.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "30m",
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successful",
+      accessToken,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured! Please try again",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser };
--- 7. nodejs-auth/server.js
+++ 7. nodejs-auth/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
 const authRoutes = require("./routes/auth-routes");
+const homeRoutes = require("./routes/home-routes");
+const adminRoutes = require("./routes/admin-routes");
 
 // initialize express app & set up middleware
 const app = express();
@@ -12,6 +14,8 @@ connectToDB();
 
 // routes
 app.use("/api/auth", authRoutes);
+app.use("/api/home", homeRoutes);
+app.use("/api/admin", adminRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
