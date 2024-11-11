63d9ef1a38def1897dc4479187f667c5c045ccbc
Luis <solarluiso@gmail.com>
2024-11-11 12:02:10 -0500

    file upload with cloudinary and multer

--- 7. nodejs-auth/server.js
+++ 7. nodejs-auth/server.js
@@ -4,6 +4,7 @@ const connectToDB = require("./database/db");
 const authRoutes = require("./routes/auth-routes");
 const homeRoutes = require("./routes/home-routes");
 const adminRoutes = require("./routes/admin-routes");
+const uploadImageRoutes = require("./routes/image-routes");
 
 // initialize express app & set up middleware
 const app = express();
@@ -16,6 +17,7 @@ connectToDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/home", homeRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/image", uploadImageRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
