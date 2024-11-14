f3d3793b935cbbe3b8cddb830191f93edf68ef06
Luis <solarluiso@gmail.com>
2024-11-14 16:59:01 -0500

    change password with auth middleware

--- 7. nodejs-auth/controllers/auth-controller.js
+++ 7. nodejs-auth/controllers/auth-controller.js
@@ -105,4 +105,53 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser };
+// change password
+const changePassword = async (req, res) => {
+  try {
+    const userId = req.userInfo.userId;
+
+    // extract old and new password;
+    const { oldPassword, newPassword } = req.body;
+
+    // find the current logged in user
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // check if the old password is correct
+    const isPasswordMatch = await bcrypt.compare(oldPassword, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Old password is not correct! Please try again.",
+      });
+    }
+
+    // hash the new password here
+    const salt = await bcrypt.genSalt(10);
+    const newHashedPassword = await bcrypt.hash(newPassword, salt);
+
+    // update user password
+    user.password = newHashedPassword;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Password changed successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured! Please try again",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser, changePassword };
