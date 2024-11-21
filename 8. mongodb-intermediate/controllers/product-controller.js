7c82ad976bf44b60a62320acf25140fa35696a1e
Luis <solarluiso@gmail.com>
2024-11-20 22:01:58 -0500

    common aggregation operators

--- 8. mongodb-intermediate/controllers/product-controller.js
+++ 8. mongodb-intermediate/controllers/product-controller.js
@@ -41,7 +41,7 @@ const insertSampleProducts = async (req, res) => {
     ];
     const result = await Product.insertMany(sampleProducts);
     res.status(201).json({
-      succes: true,
+      success: true,
       data: `Inserted ${result.length} sample products`,
     });
   } catch (e) {
@@ -53,4 +53,94 @@ const insertSampleProducts = async (req, res) => {
   }
 };
 
-module.exports = { insertSampleProducts };
+const getProductStats = async (req, res) => {
+  try {
+    const result = await Product.aggregate([
+      // stage 1
+      {
+        $match: {
+          inStock: true,
+          price: {
+            $gte: 100,
+          },
+        },
+      },
+      // stage 2 : group documents
+      {
+        $group: {
+          _id: "$category",
+          avgPrice: {
+            $avg: "$price",
+          },
+          count: {
+            $sum: 1,
+          },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+const getProductAnalysis = async (req, res) => {
+  try {
+    const result = await Product.aggregate([
+      {
+        $match: {
+          category: "Electronics",
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalRevenue: {
+            $sum: "$price",
+          },
+          averagePrice: {
+            $avg: "$price",
+          },
+          maxProductPrice: {
+            $max: "$price",
+          },
+          minProductPrice: {
+            $min: "$price",
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          totalRevenue: 1,
+          averagePrice: 1,
+          maxProductPrice: 1,
+          minProductPrice: 1,
+          priceRange: {
+            $subtract: ["$maxProductPrice", "$minProductPrice"],
+          },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+module.exports = { insertSampleProducts, getProductStats, getProductAnalysis };
