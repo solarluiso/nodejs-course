99c2f90f1e4d2157ebd47ac23ca2e94f17b94972
Luis <solarluiso@gmail.com>
2024-12-02 10:19:35 -0500

    implement real-time chat functionality with user join/leave notifications and message broadcasting

--- 9. nodejs-socket/server.js
+++ 9. nodejs-socket/server.js
@@ -9,6 +9,47 @@ const server = http.createServer(app);
 // initiate socket.io and attach it to the http server
 const io = socketIo(server);
 
+app.use(express.static("public"));
+
+const users = new Set();
+
+io.on("connection", (socket) => {
+  console.log("A user is now connected");
+
+  // handle users when they join the chat
+  socket.on("join", (userName) => {
+    users.add(userName);
+    socket.userName = userName;
+
+    // broadcast to all clients/users that a new user has joined
+    io.emit("userJoined", userName);
+
+    // send the updated user list to all clients
+    io.emit("userList", Array.from(users));
+  });
+
+  // handle incoming chat message
+  socket.on("chatMessage", (message) => {
+    // broadcast the received message to all connected clients
+    io.emit("chatMessage", message);
+  });
+
+  // handle user disconnection
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected:", socket.userName);
+
+    users.forEach((user) => {
+      if (user === socket.userName) {
+        users.delete(user);
+
+        io.emit("userLeft", user);
+
+        io.emit("userList", Array.from(users));
+      }
+    });
+  });
+});
+
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is now running on http://localhost:${PORT}`);
