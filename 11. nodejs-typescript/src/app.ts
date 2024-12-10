f2a581fb7c04f32384755377115430afe1ea0b7c
Luis <solarluiso@gmail.com>
2024-12-10 10:37:45 -0500

    set up basic Express server with TypeScript and custom request middleware

--- 11. nodejs-typescript/src/app.ts
+++ 11. nodejs-typescript/src/app.ts
@@ -1,1 +1,24 @@
-console.log("magnifico");
+import express, { Express, Request, Response, NextFunction } from "express";
+
+const app: Express = express();
+const port = 3000;
+
+app.use(express.json());
+
+interface CustomRequest extends Request {
+  startTime?: number;
+}
+
+// middleware -> add startTime to request
+app.use((req: CustomRequest, res: Response, next: NextFunction) => {
+  req.startTime = Date.now();
+  next();
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello. Typescript with express");
+});
+
+app.listen(port, () => {
+  console.log(`Server is now running on port ${port}`);
+});
